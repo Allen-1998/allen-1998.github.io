739393cd63008ffd0e70fbde3b4d00a6a512cb8b
allen <272970666@qq.com>
2022-05-18 01:28:20 +0800

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div absolute bottom-5 left-0 right-0 text-center select-none op30 fw300 text-sm>
+  <div absolute bottom-5 left-0 right-0 text-center select-none op70 fw300 text-sm>
     <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" color-inherit
       >CC BY-NC-SA 4.0</a
     >
